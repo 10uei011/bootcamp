ff9cf2025a3d7d16b9febb12e4a9297534cb6d5b
dipesh singh <dipesh.singh2012@gmail.com>
2017-03-26 20:49:49 +0530

    Fixed indentation errors

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'PetLove.com' });
+  res.render('index', { title: 'TTN Bootcamp' });
 });
 
 module.exports = router;
